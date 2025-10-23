7b37c24ca54c4121f345dd0ef3a70dfd8c01389d
leonidaspedro <leonidas@unochapeco.edu.br>
2025-10-23 15:13:57 -0300

    theme

---
+++ lua/plugins/vim-fugitive.lua
@@ -0,0 +1,12 @@
+   return {
+     {
+       "tpope/vim-fugitive",
+       cmd = { "G", "Git" },
+       keys = {
+         { "<leader>gs", "<cmd>Git<cr>", desc = "Git status" },
+         { "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
+         { "<leader>gp", "<cmd>Git push<cr>", desc = "Git push" },
+         { "<leader>gl", "<cmd>Git pull<cr>", desc = "Git pull" },
+       },
+     },
+   }
